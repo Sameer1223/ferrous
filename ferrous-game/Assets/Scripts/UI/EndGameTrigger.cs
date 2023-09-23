46f1b641733cc57b36b2f59031e4e5cb76df1f72
Lew <18721739579@163.com>
2023-09-23 18:35:00 -0400

    Added End Game UI

---
+++ ferrous-game/Assets/Scripts/UI/EndGameTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject EngTrigger;
+    public GameObject BackPanel;
+    public GameObject Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        BackPanel.SetActive(true) ;
+        Player.GetComponent<CameraController>().enabled = false;
+        Player.GetComponent<PlayerController>().enabled = false;
+        Cursor.lockState = CursorLockMode.None; // Lock the cursor to the center of the screen
+        Cursor.visible = true; //
+    }
+
+}
