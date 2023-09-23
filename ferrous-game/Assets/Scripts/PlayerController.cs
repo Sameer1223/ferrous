243a3adf14e1aa699f22b94ade3b65c845669b09
Sameer Khan <33437318+Sameer1223@users.noreply.github.com>
2023-09-23 12:43:45 -0400

    Removed camera movement from player controller script

--- ferrous-game/Assets/Scripts/PlayerController.cs
+++ ferrous-game/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,6 @@ public class PlayerController : MonoBehaviour
     public float mouseSensitivity = 2.0f;
 
     private Rigidbody rb;
-    private float verticalRotation = 0;
-
-    private Camera playerCamera;
-
     private bool isGrounded;
 
     void Start()
@@ -21,7 +17,6 @@ public class PlayerController : MonoBehaviour
         Cursor.visible = false; // Hide the cursor
 
         rb = gameObject.GetComponent<Rigidbody>();
-        playerCamera = GetComponentInChildren<Camera>();
     }
 
     void Update()
@@ -39,14 +34,6 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Jump");
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
-
-        // Player Look (Mouse)
-        float horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
-        transform.Rotate(0, horizontalRotation, 0);
-
-        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
-        playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
     }
 
     private void OnCollisionEnter(Collision other)
