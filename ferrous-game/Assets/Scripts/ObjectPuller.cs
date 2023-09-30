7c911a041cca6cb342a39486b342cde0fc81bf27
Edward Su <edward.su29@gmail.com>
2023-09-30 15:48:44 -0400

    WIP: select working, need to discuss how we want to move forwards with this feature

--- ferrous-game/Assets/Scripts/ObjectPuller.cs
+++ ferrous-game/Assets/Scripts/ObjectPuller.cs
@@ -12,20 +12,54 @@ public class ObjectPuller : MonoBehaviour
 
     private Rigidbody rb;
     private Camera mainCamera;
-    private Transform interactableObject;
+    private Transform _playerTransform;
     private bool soundPlayed;
 
+    [Header("Select")]
+    public float rayDist;
+
+    [Header("Push/Pull")]
     public float pullForce = 10f;
+    private Rigidbody selectedObject;
+    private bool isSelected;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         mainCamera = Camera.main;
+        _playerTransform = GameObject.Find("Player").transform;
     }
 
     void Update()
     {
+        selectObject();
+        if (isSelected)
+        {
+            getPushPullInput();
+        }
 
+    }
+    private void LateUpdate()
+    {
+        if (isPulling && isSelected)
+        {
+            Debug.Log("Pulling");
+            Vector3 pullDirection = (_playerTransform.position - selectedObject.position).normalized;
+            pullDirection = new Vector3(pullDirection.x, 0f, pullDirection.z);
+            selectedObject.GetComponent<Rigidbody>().AddForce(pullDirection * pullForce);
+        }
+        else if (isPushing && isSelected)
+        {
+            Debug.Log("Pushing");
+            Vector3 pushDirection = -(mainCamera.transform.position - selectedObject.position).normalized;
+            Debug.Log(pushDirection);
+            Debug.DrawRay(mainCamera.transform.position, pushDirection, Color.red);
+            pushDirection = new Vector3(pushDirection.x, 0f, pushDirection.z);
+            selectedObject.GetComponent<Rigidbody>().AddForce(pushDirection * pullForce);
+        }
+    }
+    private void selectObject()
+    {
 
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -33,61 +67,68 @@ public class ObjectPuller : MonoBehaviour
             // generates a ray in the look direction
             Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             // instead of origin -> destination, use the defined ray
-            if (Physics.Raycast(ray, out hit, rayDist)) 
+            if (Physics.Raycast(ray, out hit, rayDist))
             {
                 if (hit.collider.CompareTag("Metal"))
                 {
-                    Debug.Log("Selected Metal");
+                    GameObject prevSelectedObject;
+                    if (selectedObject != null)
+                    {
+                        // compare prev and new selected
+                        prevSelectedObject = selectedObject.gameObject;
+                        selectedObject = hit.rigidbody;
+                        if (Object.ReferenceEquals(prevSelectedObject, selectedObject.gameObject))
+                        {
+                            // de-select the object
+                            selectedObject.useGravity = true;
+                            selectedObject = null;
+                            isSelected = false;
+                        }
+                        else
+                        {
+                            // turn gravity back on for the previous object
+                            prevSelectedObject.GetComponent<Rigidbody>().useGravity = true;
+                            // make the current object hover
+                            selectedObject.useGravity = false;
+                            selectedObject.AddForce(Vector3.up * 75f);
+                        }
+                    }
+                    else
+                    {
+                        // no object was previously selected, select the current one
+                        selectedObject = hit.rigidbody;
+                        selectedObject.useGravity = false;
+                        selectedObject.AddForce(Vector3.up * 75f);
+                        isSelected = true;
+                    }
+
+
                 }
             }
         }
+    }
 
-        if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) // Change this to your desired input method
+    private void getPushPullInput()
+    {
         if (Input.GetButton("Fire1") || Input.GetAxisRaw("Fire1") > 0 || Input.GetButton("Fire2") || Input.GetAxisRaw("Fire2") > 0)
         {
-            if (!soundPlayed)
-            {
-                GunActiveSFX.Play();
-                soundPlayed = true;
-            }
-            RaycastHit hit;
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(transform.position, transform.forward, Color.green);
+            if (Input.GetMouseButton(0) || Input.GetAxisRaw("Fire1") > 0.1f) { isPulling = true; Debug.Log("pulling"); }
+            else if (Input.GetMouseButton(1) || Input.GetAxisRaw("Fire2") > 0.1f) { isPushing = true; }
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider.CompareTag("Metal"))
-                {
-                    Debug.Log(Input.GetMouseButton(0));
-                    if (Input.GetMouseButton(0) || Input.GetAxisRaw("Fire1") > 0.1f) { isPulling = true; }
-                    else if (Input.GetMouseButton(1) || Input.GetAxisRaw("Fire2") > 0.1f) { isPushing = true; }
-                    interactableObject = hit.transform;
-                }
-            }
+            
         }
 
-        if (Input.GetAxisRaw("Fire1") < 0.1f && !Input.GetMouseButton(0)) 
-        { 
+        if (Input.GetAxisRaw("Fire1") < 0.1f && !Input.GetMouseButton(0))
+        {
+            Debug.Log("not pulling");
             isPulling = false;
             soundPlayed = false;
-        } 
-        if (Input.GetAxisRaw("Fire2") < 0.1f && !Input.GetMouseButton(1)) 
-        { 
+        }
+        if (Input.GetAxisRaw("Fire2") < 0.1f && !Input.GetMouseButton(1))
+        {
+            Debug.Log("not pushing");
             isPushing = false;
             soundPlayed = false;
-        }
-
-        if (isPulling)
-        {
-            Debug.Log("Pulling");
-            Vector3 pullDirection = (mainCamera.transform.position - interactableObject.position).normalized;
-            interactableObject.GetComponent<Rigidbody>().AddForce(pullDirection * pullForce);
-        }   
-        else if (isPushing)
-        {
-            Debug.Log("Pushing");
-            Vector3 pullDirection = -(mainCamera.transform.position - interactableObject.position).normalized;
-            interactableObject.GetComponent<Rigidbody>().AddForce(pullDirection * pullForce);
         }
     }
 }
