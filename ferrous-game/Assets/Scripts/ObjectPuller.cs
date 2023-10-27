d93a5bc35058a5c29b848632e30dc7844b6688c5
Sameer Khan <33437318+Sameer1223@users.noreply.github.com>
2023-10-27 13:44:07 -0400

    Revert "getting linked to work again, fixing direction"
    This reverts commit b468418714502671a61e885bdc2f5cf23684c2db.

--- ferrous-game/Assets/Scripts/ObjectPuller.cs
+++ ferrous-game/Assets/Scripts/ObjectPuller.cs
@@ -264,11 +264,12 @@ public class ObjectPuller : MonoBehaviour
 
                 GetInteractDirectionNormalized(pullDirection);
 
-                if (linked != null)
+                selectedObject.AddForce(pullDirection * pullForce * pullMultiplier);
+                if (linkedObj != null)
                 {
                     linkedObj.GetComponent<Rigidbody>().AddForce(pullDirection * pullForce * pullMultiplier);
                 }
-                selectedObject.AddForce(pullDirection * pullForce * pullMultiplier);
+
                 SetModelColour(Color.cyan);
             }
             else if (isPushing)
@@ -279,14 +280,14 @@ public class ObjectPuller : MonoBehaviour
 
                 Vector3 pushDirection = -(mainCamera.transform.position - selectedObject.position).normalized;
                 pushDirection = new Vector3(pushDirection.x, pushDirection.y, pushDirection.z);
-                
+
                 GetInteractDirectionNormalized(pushDirection);
 
-                if (linked != null)
-                {
-                    linked.linkedObj.AddForce(pushDirection * pullForce * pushMultiplier);
-                }
                 selectedObject.AddForce(pushDirection * pullForce * pushMultiplier);
+                if (linkedObj != null)
+                {
+                    linkedObj.GetComponent<Rigidbody>().AddForce(pushDirection * pullForce * pullMultiplier);
+                }
                 SetModelColour(Color.red);
             }
         }
