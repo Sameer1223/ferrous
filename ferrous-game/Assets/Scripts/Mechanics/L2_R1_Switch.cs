3facb4e0214ff123954ab0eabed848e2f4ca3c90
Lew <18721739579@163.com>
2023-10-27 01:50:52 -0400

    Fixed issues
    fixed problems when merging develop into tut-level branch

--- ferrous-game/Assets/Scripts/Mechanics/L2_R1_Switch.cs
+++ ferrous-game/Assets/Scripts/Mechanics/L2_R1_Switch.cs
@@ -17,16 +17,16 @@ public class L2_R1_Switch : MonoBehaviour
         
     }
 
-    void OnCollisionEnter(Collision collision)
+    
+    private void OnTriggerEnter(Collider collision)
     {
-
         if (collision.gameObject.tag == "Metal")
         {
             L2_R_Switch.EndCollide2 = true;
         }
-
     }
-    private void OnCollisionExit(Collision collision)
+
+    private void OnTriggerExit(Collider collision)
     {
         L2_R_Switch.EndCollide2 = false;
     }
--- ferrous-game/Assets/Scripts/Mechanics/L2_R_Switch_Single.cs
+++ ferrous-game/Assets/Scripts/Mechanics/L2_R_Switch_Single.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class L2_R_Switch : MonoBehaviour
+public class L2_R_Switch_Single : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject R1_Door_L;
@@ -19,29 +19,27 @@ public class L2_R_Switch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(EndCollide1 && EndCollide2 && R1_Door_L.transform.position.z> -4.5f && R1_Door_R.transform.position.z<3.1f)
+        if(EndCollide1 && R1_Door_L.transform.position.z> -4.5f && R1_Door_R.transform.position.z<3.1f)
         {
             R1_Door_L.transform.position -= direction * speed * Time.deltaTime;
             R1_Door_R.transform.position += direction * speed * Time.deltaTime;
         }
     }
-    void OnCollisionEnter(Collision collision)
+
+    private void OnTriggerEnter(Collider collision)
     {
-        
-        if(collision.gameObject.tag == "Metal")
+        if (collision.gameObject.tag == "Metal")
         {
             EndCollide1 = true;
         }
-        
-    }
-    void OnCollisionExit(Collision collision)
-    {
 
-        EndCollide1 = false;
     }
-
-    public void SetEndCollided2True()
+    private void OnTriggerExit(Collider collision)
     {
-        EndCollide2 = true;
+        if (collision.gameObject.tag == "Metal")
+        {
+            EndCollide1 = false;
+        }
+
     }
 }
