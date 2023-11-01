534f2609360c43dcd8c68cbf6a810f49324f7b29
Edward Su <edward.su29@gmail.com>
2023-11-01 01:08:09 -0400

    commiting to look at kerry's branch

--- ferrous-game/Assets/Scripts/UI/LevelComplete.cs
+++ ferrous-game/Assets/Scripts/UI/LevelComplete.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class LevelComplete : MonoBehaviour
 {
+
+    [Header("Possible First Btns")]
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,15 @@ public class LevelComplete : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            // if keyboard / gamepad input, select the first thing in the list
+            if ((Keyboard.current != null && Keyboard.current.anyKey.wasPressedThisFrame) ||
+                (Gamepad.current != null && Gamepad.current.allControls.Any(control => control.IsActuated()))
+                )
+            {
+                EventSystem.current.SetSelectedGameObject(_firstBtn);
+            }
+        }
     }
 }
